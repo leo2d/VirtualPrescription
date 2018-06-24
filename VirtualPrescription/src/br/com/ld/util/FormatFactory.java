927717608afe51e391edd0ce7fb5c0f2d89e5e58
Leonardo Dias <contato.leonardod@yahoo.com>
2018-06-23 23:29:05 -0300

    Correcao na transformacao de data

--- VirtualPrescription/src/br/com/ld/util/FormatFactory.java
+++ VirtualPrescription/src/br/com/ld/util/FormatFactory.java
@@ -9,23 +9,22 @@ package br.com.ld.util;
  *
  * @author Leonardo
  */
-
 import java.text.DecimalFormat;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class FormatFactory {
-    
-    public static String formatDate(Date data){
-    
-        SimpleDateFormat sdf = new SimpleDateFormat("DD/MM/YYYY");
-        
-        return  sdf.format(data);
+
+    public static String formatDate(Date data) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        return sdf.format(data);
     }
-    
-    public static String formatDecimal(double valor){
+
+    public static String formatDecimal(double valor) {
         DecimalFormat df = new DecimalFormat("#0.00");
-        
+
         return df.format(valor);
     }
 }
